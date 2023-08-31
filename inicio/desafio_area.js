fc6c9c1672e9ef70db5d7259a9cd4ae2a5338d98
Giovane Antonio de paiva <giovane-a.p@hotmail.com>
2023-08-31 00:18:39 -0300

    .

---
+++ inicio/desafio_area.js
@@ -0,0 +1,6 @@
+const PI = 3.141592;
+let raio = 10;
+let resultado = PI * raio * raio;
+
+
+console.log("O valor da area dá: "+ resultado+"m2");
---
+++ inicio/desafio_troca.js
@@ -0,0 +1,8 @@
+let a = 7;
+let b = 94;
+ console.log(a)
+ console.log(b)
+a=94
+b=7
+console.log(a)
+ console.log(b)
