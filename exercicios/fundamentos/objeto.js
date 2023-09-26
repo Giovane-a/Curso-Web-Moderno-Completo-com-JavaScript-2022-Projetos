afe02c30697fd98b42ab3f741337274bc7bea289
Giovane Antonio de paiva <giovane-a.p@hotmail.com>
2023-09-25 23:47:34 -0300

    .

---
+++ exercicios/fundamentos/objeto.js
@@ -0,0 +1,23 @@
+const prod1 = {}
+
+prod1.nome = 'Celular ultra mega'
+
+prod1.preco= 4998.90
+
+prod1['desconto legal']= 0.40
+
+console.log(prod1)
+
+
+const prod2 = {
+    nome: 'Camisa polo',
+
+    preco: 79.90, 
+    obj:{
+        categoria: 1,
+        obj:{ tipo :1
+        }
+    }
+}
+
+console.log(prod2)
